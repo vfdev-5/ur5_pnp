f88f0bc0886a0dc34adf4fdc8895f4bdd1d674fe
vfdev-5 <vfdev.5@gmail.com>
2018-08-13 22:24:42 +0200

    Add run robot script to launch all

---
+++ run_robot.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+TERM=xterm
+CD=$PWD
+
+# Start ROS Core
+$TERM -e "source $CD/devel/setup.bash &&  roscore" &
+
+sleep 5
+
+# Bring up robot
+$TERM -e "source $CD/devel/setup.bash && roslaunch  ur_modern_driver ur5_bringup.launch limited:=true robot_ip:=192.168.0.34"  &
+
+sleep 5
+
+# Start moveit planning execution 
+$TERM -e "source $CD/devel/setup.bash && roslaunch ur5_moveit_config ur5_moveit_planning_execution.launch limited:=true" &
